17b53012dfad43e6890db12ac17bba6e83565c75
shieldstring <edmunddaniels06@gmail.com>
2020-09-26 16:43:19 +0100

    completed challenge 1 and 2, working on 3.

---
+++ static/js/script.js
@@ -0,0 +1,25 @@
+function ageInDays(){
+    var birthyear = prompt('what year were you born?');
+    var ageInDayss = (2020 - birthyear) * 365;
+    var h1 = document.createElement('h1');
+    var textAnswer = document.createTextNode('You are ' + ageInDayss + ' days');
+    h1.setAttribute('id', 'ageInDays');
+    h1.appendChild(textAnswer);
+    document.getElementById('flex-box-result').appendChild(h1);
+}
+
+function reset(){
+    document.getElementById('ageInDays').remove();
+}
+
+// challenge 2: cat generator
+function generateCat(){
+    var image = document.createElement('img');
+    var div = document.getElementById('flex-cat-gen');
+    image.src ="https://api.thecatapi.com/api/images/get?format=src&type=gif&size=small"
+    div.appendChild(image);
+}
+
+
+// chanllenge 3: Rock, papper and scissor
+ 
